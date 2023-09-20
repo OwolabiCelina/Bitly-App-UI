4d53a16ce2b37f9b6f9ef8f724f3121b496ad16c
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-09-20 13:29:49 +0100

    blue colored header added

---
+++ src/blueheader/BlueHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './blueheader.css'
+
+const BllueHeader = () => {
+  return (
+    <div>
+        <div className='blue-header-container'>
+            <p><span id='new'>NEW</span> Get fresh insights into QR Code trends in 2023 and beyond. <a href="./">Read the report</a></p>
+        </div>
+    </div>
+  )
+}
+
+export default BllueHeader
